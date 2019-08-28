5394cf16250823796f44ff71efd097562a40e38d
Hayden Wolff <haydenwolff99@gmail.com>
2019-08-28 15:47:14 -0700

    added files

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var app = new express();
+// const app = express()
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+  // .get('/', (req, res) => res.render('pages/index'))
+app.get('/cool', (req, res) => res.send(cool()))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-const app = express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get("/", function (request, response) {
+    response.sendFile('/public/index.html', {root: __dirname});
+});
+
+function openDrawerMenu(){
+  var x = document.getElementById("mainNavBar");
+  if (x.className === "navBar"){
+    x.className += " responsive";
+  } else {
+    x.className = "navBar";
+  }
+}
