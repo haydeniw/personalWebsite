75fdd248e80c3dcbc128462569f175b73c4b2161
Hayden Wolff <haydenwolff99@gmail.com>
2019-08-28 17:04:34 -0700

    got rid of cors

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 var app = new express();
-var cors = require('cors');
 
 app.get("/", function (request, response) {
     response.sendFile('/public/index.html', {root: __dirname});
@@ -9,3 +8,5 @@ app.get("/", function (request, response) {
 app.get("/public/stylesheets/style.css", function (req, resp) {
     resp.sendFile('/public/stylesheets/style.css', {root: __dirname});
 });
+
+app.listen(process.env.PORT || 8888);
